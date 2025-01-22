d51a02c2c70ab5529be1059f2b25e3b505ae1443
Gk241196 <gk527@192.168.1.34>
2025-01-22 16:00:10 +0530

    Code added

---
+++ Testleafselenium/src/Test1.java
@@ -0,0 +1,9 @@
+
+public class Test1 {
+	public static void main(String args[]) {
+	 String input = "Ganesh kumar";
+	 System.out.println(input);
+	 String input1 = "Ganesh kumar";
+	 System.out.println(input1);
+	}
+}
