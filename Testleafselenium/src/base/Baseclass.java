d1b64aebce6d1d7ffe7c87203e221748691992e9
'GK241196 <Gk52707@gmai.com>
2025-01-25 22:41:24 +0530

    second code change

--- Testleafselenium/src/base/Baseclass.java
+++ Testleafselenium/src/base/Baseclass.java
@@ -1,5 +1,25 @@
 package base;
 
-public class Baseclass {
+import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+
+public class Baseclass {
+  WebDriver driver;
+    @BeforeTest
+    public void browserlaunch() {
+        driver = new ChromeDriver();
+        driver.get("https://www.leafground.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @AfterMethod
+    public void tearDown() {
+            driver.quit();
+        }
+    
 }
+
